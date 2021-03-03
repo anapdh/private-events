c090068b893f13722a314ea8fd95e5fc709f57ab
jpdf00 <j.pdf00@gmail.com>
2021-03-03 17:44:58 -0300

    Add event functionality dependent on user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,7 +21,7 @@ class EventsController < ApplicationController
 
   # POST /events or /events.json
   def create
-    @event = Event.new(event_params)
+    @event = current_user.created_events.build(event_params)
 
     respond_to do |format|
       if @event.save
