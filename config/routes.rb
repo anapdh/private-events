5c81718ee12132abdd05a6847c4d6596b037ac31
jpdf00 <j.pdf00@gmail.com>
2021-03-03 15:55:07 -0300

    Add users#show functionality with routes, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events
-  root to: "home#index"
+  root to: "events#index"
+
+  get "/users/:id", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
