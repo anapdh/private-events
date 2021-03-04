223a3a19a4f0d787c45b4d4fdcfe99fad04006dc
anapdh <ana_d_hubner@hotmail.com>
2021-03-04 16:03:36 -0300

    make User event attendance and index events display as upcoming and past events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,10 @@ class EventsController < ApplicationController
 
   # GET /events or /events.json
   def index
-    @events = Event.all
+    @upcoming_events = Event.where("date > ?", Time.now)
+    @past_events = Event.where("date < ?", Time.now)
+    # @past_events = Event.past_events
+    # @future_events = Event.future_events
   end
 
   # GET /events/1 or /events/1.json
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   has_many :event_attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendances, source: :attendee
   belongs_to :creator, class_name: "User"
+
+  # def self.past_events
+  #   self.where(datetime < Time.now)
+  # end
+
+  # def self.future_events
+  #   self.where(datetime > Time.now)
+  # end
 end
