3a95120df68cf7712646592d03677fc72b557276
anapdh <ana_d_hubner@hotmail.com>
2021-03-05 14:29:21 -0300

    add rspec for event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   
   validates :name, :date, :location, presence: true
-  validates :description, presence: true, length: { maximum: 12_000 }
+  validates :description, presence: true, length: { minimum: 3 }
 
   scope :upcoming, -> { where("date > ?", Time.now) }
   scope :past, -> { where("date < ?", Time.now) }
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -7,3 +7,43 @@ RSpec.describe Event, :type => :model do
     it { should belong_to(:creator).class_name('User') }
   end
 end
+
+RSpec.describe Event, :type => :model do
+  user { User.new(id: 1, name:"Foo", username:"foozin", email:"foo@zin.com", password:"123456") }
+  subject {
+    Event.new(name: "Anything",
+                        description: "Lorem ipsum",
+                        date: DateTime.now,
+                        location: "USA",
+                        creator_id: user.id)
+  }
+
+  it "is valid with name, description, date and location" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without minimum of 3 characters in description" do
+    subject.description = "hi"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a date" do
+    subject.date = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a location" do
+    subject.location = nil
+    expect(subject).to_not be_valid
+  end
+end
